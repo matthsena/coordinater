b4c1cc5b245c0b8c353b934b9ee5b7b0ab4b94c9
Matheus <matheusalexandre.sena99@gmail.com>
2026-03-18 23:22:57 -0300

    feat: screenshot and a new size get lib

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use screen_size::get_primary_screen_size;
 use xcap::Monitor;
 
 fn main() {
     let mut width = 0;
     let mut height = 0;
-    if let Ok((w, h)) = get_primary_screen_size() {
-        width = w;
-        height = h;
-    }
-    print!("{} {}", width, height);
-    // Screenshots
     let displays = Monitor::all().unwrap();
     if let Some(primary) = displays.first() {
+        if let Ok(w) = primary.width()
+            && let Ok(h) = primary.height()
+        {
+            width = w;
+            height = h;
+        }
         let screenshot = primary.capture_image().unwrap();
         screenshot.save("screenshot.png").unwrap();
     }
+    println!("{} x {} size", width, height);
 }
