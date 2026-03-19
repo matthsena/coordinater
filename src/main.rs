79a229451c68c0f1fa7ba6ebd27fdc33ddaa746f
Matheus <matheusalexandre.sena99@gmail.com>
2026-03-18 23:38:16 -0300

    feat: drawing lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use image::Rgba;
+use imageproc::drawing::draw_line_segment_mut;
 use xcap::Monitor;
 
 fn main() {
@@ -11,7 +13,13 @@ fn main() {
             width = w;
             height = h;
         }
-        let screenshot = primary.capture_image().unwrap();
+        let mut screenshot = primary.capture_image().unwrap();
+        let cx = width as f32 / 2.0;
+        let cy = height as f32 / 2.0;
+        let red = Rgba([255u8, 0, 0, 1]);
+        draw_line_segment_mut(&mut screenshot, (0.0, cy), (width as f32, cy), red);
+        draw_line_segment_mut(&mut screenshot, (cx, 0.0), (cx, height as f32), red);
+
         screenshot.save("screenshot.png").unwrap();
     }
     println!("{} x {} size", width, height);
