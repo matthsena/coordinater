4bfb24cb75c353227d4a85c533c31da9a433c517
Matheus <matheusalexandre.sena99@gmail.com>
2026-03-18 22:45:23 -0300

    feat: getting screen size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use screen_size::get_primary_screen_size;
+use screenshots::Screen;
+
 fn main() {
-    println!("Hello, world!");
+    let mut width = 0;
+    let mut height = 0;
+    if let Ok((w, h)) = get_primary_screen_size() {
+        width = w;
+        height = h;
+    }
+
+    let screens = Screen::all().unwrap();
 }
